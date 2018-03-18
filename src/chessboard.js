6ff6e5d631964178cc07d1139b159dc71ff77a3e
Roberto Fernandes <betocostaf@gmail.com>
2018-03-17 23:23:53 -0300

    Adding function to draw queens on the board

--- src/chessboard.js
+++ src/chessboard.js
@@ -1,10 +1,12 @@
 class ChessBoard {
-    constructor(width, heigth){
+    constructor (offset, width, heigth) {
         this.width = width;
         this.heigth = heigth;
+        this.offset = offset;
+        this.queen_image = loadImage('assets/queen.png');
     }
 
-    draw(size) {
+    draw (size) {
         this.size = size;
         this.square_width = this.width / size;
         this.square_heigth = this.heigth / size;
@@ -14,14 +16,24 @@ class ChessBoard {
         for (var i = 0; i < this.size; i++) {
             for (var j = 0; j < this.size; j++){
                 fill(color);
-                rect(i * this.square_width, j * this.square_heigth, this.square_width, this.square_heigth);
+                rect((i * this.square_width) + this.offset, (j * this.square_heigth) + this.offset, this.square_width, this.square_heigth);
                 color = this.change_color(color);
             }
             color = this.change_color(color);
         }
     }
     
-    change_color(color) {
-        return (color == WHITE) ? BLACK : WHITE;
+    change_color (color) {
+        return (color == WHITE) ? GRAY : WHITE;
+    }
+
+    insert_queens (positions){
+        for (var i = 0; i< this.size; i++){
+            this.draw_queen((i * this.square_width) + this.offset, ((positions[i] - 1) * this.square_heigth) + this.offset, this.square_width, this.square_heigth);
+        }
+    }
+
+    draw_queen (x, y, width, height){
+        image(this.queen_image, x + (width / 10), y + (height / 10), width - (2 * width / 10), height - (2 * height /10), 0, 0);
     }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,17 +1,22 @@
-const canvas_width = 600;
-const canvas_heigth = 600;
+const canvas_width = 800;
+const canvas_heigth = 800;
 const board_size = 8;
+const GRAY = 170;
 const BLACK = 0;
 const WHITE = 255;
+const offset = 20;
+
+var chess_board;
 
 function setup() {
-  createCanvas(canvas_width, canvas_heigth);
+  createCanvas(canvas_width + offset, canvas_heigth + offset);
   noFill();
   noStroke();
+  chess_board = new ChessBoard(offset / 2, canvas_width, canvas_heigth, board_size);
 }
 
 function draw() {
-  background(0);
-  var chess_board = new ChessBoard(canvas_width, canvas_heigth, board_size);
+  background(127, 0, 0, 255);
   chess_board.draw(board_size);
+  chess_board.insert_queens([2, 4, 4, 6, 8, 6, 3, 2]);
 }
