dfc75291fca8632018ebdc9df62cd5eebe803472
Roberto Fernandes <betocostaf@gmail.com>
2018-03-18 13:39:48 -0300

    Show on the board selected solution

--- src/population_list.js
+++ src/population_list.js
@@ -6,13 +6,37 @@ class PopulationList{
 
         this.label = createElement('label', 'Select one candidate solution');
         this.label.attribute('for', 'population-select');
-        this.label.style('font-size', '30px');
+        this.label.addClass('header-label');
         this.label.parent(this.div);        
         
         this.select = createSelect();
         this.select.addClass("form-control");
+        this.select.addClass("population-info");
         this.select.id("population-select");
         this.select.attribute("size", "5");
         this.select.parent(this.div);
+    }
+    
+    insert_population(population, isRunning){
+        this.isRunning = isRunning;
+        if (this.isRunning){
+            $('#population-select').empty();
+            for(var i = 0; i < population.length; i++){
+                this.select.option(`${i+1}: Fitness: ${population[i].fitness}  - Positions: [${population[i].positions}]`);
+            }
+            $('#population-select').val(($("#population-select option:first").val()));
+        }
+    }
+
+    get_selected(){
+        var text = $('#population-select').val();
+        text = text.split('[')[1].split(']')[0];
+
+        var positions = text.split(',');
+        positions = positions.map((elem) => {
+            return parseInt(elem);
+        });
+
+        return positions;
     }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -7,6 +7,34 @@ const WHITE = 255;
 const offset = 20;
 
 var chess_board;
+var isRunning = true;
+
+const list = [
+  {
+    fitness: 10,
+    positions: [2, 4, 4, 6, 8, 6, 3, 2]
+  },
+  {
+    fitness: 11,
+    positions: [1, 2, 3, 4, 5, 6, 7, 8]
+  },
+  {
+    fitness: 10.5,
+    positions: [1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    fitness: 2,
+    positions: [1, 7, 8, 2, 4, 5, 8, 6]
+  },
+  {
+    fitness: 30,
+    positions: [1, 3, 5, 7, 2, 4, 6, 8]
+  },
+  {
+    fitness: 30,
+    positions: [1, 3, 5, 7, 2, 4, 6, 8]
+  }
+]
 
 function setup() {
   createCanvas(canvas_width + offset, canvas_heigth + offset);
@@ -19,5 +47,8 @@ function setup() {
 function draw() {
   background(127, 0, 0, 255);
   chess_board.draw(board_size);
-  chess_board.insert_queens([2, 4, 4, 6, 8, 6, 3, 2]);
+  population_list.insert_population(list, isRunning);
+  chess_board.insert_queens(population_list.get_selected());
+
+  isRunning = false;
 }
