8848393375a06b0c99014f0209c99d2abd1ef129
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-04-19 19:01:48 -0300

    Addind terminations method

--- src/ga_methods/termination.js
+++ src/ga_methods/termination.js
@@ -1,11 +1,27 @@
 class Termination {
     constructor() {
-
+        this.count = 0;
     }
-    find_solution(population, board_size){
-
+    find_solution(population_temp, board_size){
+        population_temp.sort(function (a, b) {
+            return a.fitness < b.fitness;
+        });
+        if(population_temp[0].fitness == 1){
+            console.log("FOUND IT");
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    fitness_evaluation(population, board_size, max_evaluation){
-
+    fitness_evaluation(calc_fitness, board_size, max_evaluation){
+        this.count += calc_fitness;
+        if(calc_fitness >= max_evaluation)
+            return true;
+        else
+            return false;        
+    }
+    clearCount(){
+        this.count = 0;
     }
 }
