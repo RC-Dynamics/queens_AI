b9b590186d96b8e6599c22187aa14d2704fa6078
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-04-19 19:02:09 -0300

    Adding selection method

--- src/ga_methods/selection.js
+++ src/ga_methods/selection.js
@@ -3,7 +3,16 @@ class Selection {
 
     }
 
-    best_fitness(population, children, board_size){
-        
+    best_fitness(population_temp, children, board_size){
+        var population_limit = population_temp.length;
+        var all_genotypes = population_temp.slice();
+        for(var i = 0; i < children.length; i++){
+            all_genotypes.push(children[i]);
+        } 
+        all_genotypes.sort(function (a, b) {
+            return a.fitness < b.fitness;
+        });
+        population_temp = all_genotypes.slice(0,population_limit);
+        console.log(population_temp[0]);
     }
 }
